f6711d04432238f4b94f0f9715b6a1dc73210b57
Broo-DevX-Coder <broodevx@gmail.com>
2025-04-21 23:23:15 +0100

    Add Restart app debugin

--- bitget.py
+++ bitget.py
@@ -3,7 +3,7 @@ import httpx
 # تحميل البيانات مرة واحدة وتخزينها داخليًا
 _cached_symbol_data = None
 
-def fetch_symbol_data():
+def fetch_symbol_data():  # this functon return 3 values ( Pairs / Error_type / Error_info )
         global _cached_symbol_data
     
         url = 'https://api.bitget.com/api/v2/spot/public/symbols'
@@ -11,37 +11,40 @@ def fetch_symbol_data():
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
         try:
-            with httpx.Client(timeout=5.0) as client:
+            with httpx.Client(timeout=10.0) as client:
                 response = client.get(url, headers=headers)
 
             if response.status_code == 200:
                 data = response.json()
                 _cached_symbol_data = data.get('data', [])
+                return _cached_symbol_data,None,None 
             else:
-                print(f"Error: {response.status_code}")
                 _cached_symbol_data = []
+            return _cached_symbol_data,"response_code",str(response.status_code)
 
         except httpx.RequestError as e:
-            print(f"Request error: {e}")
             _cached_symbol_data = []
+            return _cached_symbol_data,"request_code",str(e)
 
-        return _cached_symbol_data
+
+        
 
 
-def get_all_trading_pairs() -> list:
-    trading_pairs = fetch_symbol_data()
+def get_all_trading_pairs(): # this functon return 3 values ( Pairs / Error_type / Error_info )
+    trading_pairs,error_type,error_info = fetch_symbol_data()
     pairs_info = [
         pair['symbol']
         for pair in trading_pairs if pair['status'] == 'online' and pair['quoteCoin'] == "USDT"
     ]
-    return set(pairs_info)
+    return set(pairs_info),error_type,error_info
 
 
-def get_pair_info(symbol: str):
-    symbols = fetch_symbol_data()
+def get_pair_info(symbol: str): # this functon return 3 values ( Pairs / Error_type / Error_info )
+
+    symbols,e,r = fetch_symbol_data()
+
     for pair in symbols:
         if pair['symbol'] == symbol:
-            return pair
-
-    print(f"زوج {symbol} غير موجود في قائمة الأزواج المتاحة")
-    return None
+            return pair,None,None
+        
+    return None,"Not_found","this pair not found !!"
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import subprocess,sys
 from threading import Thread
 from dotenv import load_dotenv
 from datetime import datetime
@@ -10,25 +11,43 @@ import tkinter as tk
 
 # work functions ===================================================================================================
 def bitget_crypto(): # ===> To compare the piars of bitget
+    time.sleep(1)
+    new_listbox.insert(0, f"[SYSTEM: {datetime.now().strftime("%m/%d %H:%M:%S")}] Start the bot ")
+    new_listbox.itemconfig(0, {'fg': "black"})
+    new_listbox.insert(0, f"[SYSTEM: {datetime.now().strftime("%m/%d %H:%M:%S")}] Shosed Platform> Bitget")
+    new_listbox.itemconfig(0, {'fg':"#7AE2CF"})
     db = sqlite3.connect("Crypto.db")
     cursor = db.cursor()
     while True:
         try:
             time_start = datetime.now().timestamp()
-            new_pairs = get_all_trading_pairs()
+            new_pairs,new_pairs_error_type,new_pairs_error_info = get_all_trading_pairs()
             old_pairs = get_pair.get_symboles(g="bitget",db=db)
-            if new_pairs != [] and new_pairs != None:
+
+            
+            if new_pairs_error_type is None:
                 compare_pairs = new_pairs - old_pairs
                 request_in = datetime.now().timestamp() - time_start
-            else:
-                compare_pairs = None 
+            elif new_pairs_error_type == "response_code":    
+                new_listbox.insert(0, f"[SYSTEM: {datetime.now().strftime("%m/%d %H:%M:%S")}] Restarting ")
+                new_listbox.itemconfig(0, {'fg': "orange"}) 
+                restart_script()
+            elif new_pairs_error_type == "request_code":
+                new_listbox.insert(0, f"[SYSTEM: {datetime.now().strftime("%m/%d %H:%M:%S")}] Restarting ")
+                new_listbox.itemconfig(0, {'fg': "orange"})
+                restart_script()
+
+                
 
             if compare_pairs:
                 for f in compare_pairs:
+                    new_listbox.insert(0, f"[NOTIFICATION: {datetime.now().strftime("%m/%d %H:%M:%S")}] 🆕 Detected {f}")
+                    new_listbox.itemconfig(0, {'fg': "blue"})
                     cursor.execute( "INSERT INTO new (symbol,platform,time) VALUES (?,?,?)", (str(f) , "bitget",str(datetime.now().timestamp())) )
-                    pair_info = get_pair_info(f)
+                    pair_info,gg,r = get_pair_info(f)
                     cursor.execute("INSERT INTO bitget (symbol,status,baseAsset,quoteAsset,addtime) VALUES (?,?,?,?,?)", (str(pair_info["symbol"]),str(pair_info["status"]),str(pair_info["baseCoin"]),str(pair_info["quoteCoin"]),str(datetime.now().timestamp())))
-                    new_listbox.insert(tk.END, f"🆕 {f} Aeded in: {datetime.now().strftime("%m/%d %H:%M:%S")}")
+                    new_listbox.insert(0, f"[NOTIFICATION: {datetime.now().strftime("%m/%d %H:%M:%S")}] 🆕 Aeded {f}")
+                    new_listbox.itemconfig(0, {'fg': "green"})
                 db.commit()
 
             platform_status_speed.config(text=f"Request in: {request_in}S",fg="green" if request_in <= 1 else "orange")
@@ -37,32 +56,36 @@ def bitget_crypto(): # ===> To compare the piars of bitget
         except Exception as e:
             platform_status.config(text=f"Platform Status: error",fg="red")
             platform_status_speed.config(text=f"Request in: Error !!",fg="red")
-            notif_label.config(text=f"Status error Error !!",fg="red")
-            new_listbox.config(fg="red")
-            new_listbox.insert(tk.END, e)
+            new_listbox.insert(0, f"[SYSTEM: {datetime.now().strftime("%m/%d %H:%M:%S")}] Error>\n{e} ")
+            new_listbox.itemconfig(0, {'fg': "red"})
+            print(e)
             time.sleep(5)
+            restart_script()
 
 # Auther functions ======================================================================================================
-
+def restart_script(r:int = 5):
+    time.sleep(int(r))
+    root.quit()
+    subprocess.Popen([sys.executable]+sys.argv)
+    sys.exit()
 
 # setunges branch ===================================================================================================
 def main():
 
     # GUI start ---------------------------------------------------------------
+    global root,platform_status,platform_status_speed,new_listbox
+
     root = tk.Tk()
     root.title("status window for Bitget")
-    root.geometry("400x300")
+    root.geometry("600x300")
+    root.resizable(False,False)
     root.configure(bg="#f0f0f0")
-    global platform_status,notif_label,platform_status_speed,new_listbox
 
     platform_status = tk.Label(root, text="Platform Status: ---", font=("Arial", 14), bg="#f0f0f0")
     platform_status.pack(pady=5)
 
     platform_status_speed = tk.Label(root, text="Request in: ---", font=("Arial", 14), bg="#f0f0f0")
     platform_status_speed.pack(pady=5)
-
-    notif_label = tk.Label(root, text="New pairs:", font=("Arial", 14), bg="#f0f0f0")
-    notif_label.pack(pady=5)
 
     new_listbox = tk.Listbox(root, font=("Arial", 12), fg="green", height=10)
     new_listbox.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
@@ -75,7 +98,6 @@ def main():
 if __name__ == "__main__":
 
     # platform start ---------------------------------------------------------------
-    print("Now bitget it work ========================================================================")
     Bitget = Thread(target=bitget_crypto,daemon=True) # to start bitget
     Bitget.start()
 
